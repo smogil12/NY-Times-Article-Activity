36627b407e1a0131b47ec730610c4bfe97ea1960
Spencer Mogil <smogil12@gmail.com>
2019-11-23 13:02:58 -0500

    initial push with html, js, css

---
+++ app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+    $(".searchbutton").on("click", function(event) {
+
+
+    var searchInput = $(".searchterm").val();
+    var recordinput = $(".records").val();
+    var startYear = $(".startyear").val();
+    var endYear = $(".endyear").val();
+
+    console.log (searchInput, recordinput, startYear, endYear);
+
+    
+
+
+
+
+
+
+    });
+
+
+
+});
